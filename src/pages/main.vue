300cc9989c8f4e23a7c0ab294009c28d3a0a7da0
litieming <litieming@xiaomi.com>
2020-03-27 15:34:32 +0800

    tesr

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,6 +1,9 @@
 <template>
  <el-row>
   <NavHeader title='主标题' />
+  <el-button type="primary" @click="add">点我加一</el-button>
+  <div>初始值--->{{single}}</div>
+  <div>计算得来的双倍值--->{{double}}</div>
 </el-row>
 </template>
 
@@ -11,11 +14,26 @@ export default {
   components:{NavHeader},
   data () {
     return {
-      msg: '跳转到测试页面'
+      msg: '跳转到测试页面',
+      single:1,
     }
   },
   methods:{
-   
+    add(){
+      this.single += 1;
+    }
+  },
+  computed:{
+    double () {
+      return this.single *2
+    }
+  },
+  //当double这个值改变，就会触发这个函数
+  watch:{
+    double(){
+      console.log('---',this.double);
+
+    }
   }
 }
 </script>
